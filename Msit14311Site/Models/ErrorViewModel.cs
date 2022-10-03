3a6f59cae7114b2fb7766b53fe60a423cfcbdd94
家毅 廖 <ducklorl815@gmail.com>
2022-10-03 11:39:07 +0800

    加入專案檔案。

---
+++ Msit14311Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Msit14311Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
